94e782d839b7e51596b86db373b7259253fdc201
Unknown <pkheni@inboxmarketer.com>
2018-01-21 17:42:35 -0500

    match files to excercise files

--- test/user-api-spec.js
+++ test/user-api-spec.js
@@ -5,30 +5,30 @@ var fs = require('co-fs');
 var api = require('../user-web.js');
 var request = require('co-supertest').agent(api.listen());
 
-before(function *() {
-	yield fs.writeFile('./users.json', '[]');	
+before(function *(){
+    yield fs.writeFile('./users.json', '[]');
 });
 
-describe ('user data', function() {
-	it ('should have +1 user count after saving', function*() {
-		var users = yield data.users.get();
-		yield data.users.save({ name: 'John'});
-
-		var newUsers = yield data.users.get();
-
-		newUsers.length.should.equal(users.length + 1);
-	});
+describe('user data', function() {
+    it('should have +1 user count after saving', function* () {
+        var users = yield data.users.get();
+        
+        yield data.users.save({ name: 'John' });
+        
+        var newUsers = yield data.users.get();
+        
+        newUsers.length.should.equal(users.length + 1);
+    });
 });
 
-describe ('web data', function() {
-	it ('should have +1 user count after saving', function *() {
-		var res = yield request.get('/user').expect(200).end();
-		var users = res.body;
-		yield res.users.save({ name: 'John'});
-		var newRes = yield request.get('/user').expect(200).end();
-		var newUsers = newRes.body;
+describe('user web', function() {
+    it('should have +1 user count after saving', function* () {
+        var users = (yield request.get('/user').expect(200).end()).body;
 
-		newUsers.length.should.equal(users.length + 1);
-
-	})
+        yield data.users.save({ name: 'John' });
+        
+        var newUsers = (yield request.get('/user').expect(200).end()).body;
+        
+        newUsers.length.should.equal(users.length + 1);
+    });
 });
