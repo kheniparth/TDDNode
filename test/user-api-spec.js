016fe99c182bd0667ff196f6a65ff55b9985cf13
Unknown <pkheni@inboxmarketer.com>
2018-01-21 17:35:27 -0500

    add web layer

--- test/user-api-spec.js
+++ test/user-api-spec.js
@@ -2,6 +2,8 @@ require('co-mocha');
 var should = require('should');
 var data = require('../user-data.js');
 var fs = require('co-fs');
+var api = require('../user-web.js');
+var request = require('co-supertest').agent(api.listen());
 
 before(function *() {
 	yield fs.writeFile('./users.json', '[]');	
@@ -16,4 +18,17 @@ describe ('user data', function() {
 
 		newUsers.length.should.equal(users.length + 1);
 	});
+});
+
+describe ('web data', function() {
+	it ('should have +1 user count after saving', function *() {
+		var res = yield request.get('/user').expect(200).end();
+		var users = res.body;
+		yield res.users.save({ name: 'John'});
+		var newRes = yield request.get('/user').expect(200).end();
+		var newUsers = newRes.body;
+
+		newUsers.length.should.equal(users.length + 1);
+
+	})
 });
